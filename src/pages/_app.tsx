e3a35ab478f17f0df1cb56e71882a5178eb54881
Номин-Эрдэнэ Золбаяр <HP236384@G302-CC0118.local>
2023-12-19 19:47:04 -0800

    Add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>biy daalt deegii bagshid</title>
+        <meta name="description" content="biy daalt deegii bagshid" />
+        <meta name="author" content="Nominerdene" />
+        <meta name="og:type" content="card" />
+        <meta name="og:title" content="biy daalt deegii bagshid" />
+        <meta name="og:description" content="biy daalt deegii bagshid" />
+        <meta name="og:image" content="/zurag.jpeg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/fav/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/fav/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/fav/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/fav/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 
+
 export default function Home() {
   const [isImageVisible, setIsImageVisible] = useState(false);
 
@@ -9,32 +10,48 @@ export default function Home() {
   };
 
   return (
-    <div className="flex justify-center column h-screen grid place-items-center bg-gradient-to-r from-pink-400 to-black ">
+    <div className="flex flex-row justify-center h-screen grid place-items-center bg-gradient-to-r from-blue-300 to-black ">
       {isImageVisible && (
         <Image
-          className="flex absolute z-50 justify-center mb-200 "
+          className="flex absolute z-50 justify-center mt-[-370px] mr-[-10px]"
           src={"/kiss.png"}
           alt="kiss"
-          width={50}
+          width={70}
           height={400}
         />
       )}
+      <Image
+        className="border-solid border-2 border-white rounded-full"
+        alt={"zurag"}
+        src={"/zurag.jpeg"}
+        width={400}
+        height={400}
+      />
+      <div className="flex flex-col items-center justify-center">
+        <button
+          className="bg-blue-500 w-[100px] rounded-full h-[50px] text-2xl mt-[-320px] hover:bg-blue-700"
+          onClick={handleClick}
+        >
+          {isImageVisible ? "Дар" : "Дар"}
+        </button>
+        <a
+          className="w-[100px] flex-row ml-[30px] mt-[70px]"
+          href="https://www.facebook.com/profile.php?id=100003806587648"
+        >
+          <Image
+            // className="border-solid border-2 border-white rounded-full"
+            alt={"zurag"}
+            src={"/facebook.png"}
+            width={60}
+            height={100}
+          />
+          <p className="mb-[30px]">Facebook</p>
+        </a>
 
-      <a href="https://www.facebook.com/profile.php?id=100003806587648">
-        <Image
-          className="border-solid border-2 border-white rounded-full"
-          alt={"zurag"}
-          src={"/zurag.jpeg"}
-          width={400}
-          height={900}
-        />
-      </a>
-      <button
-        className="bg-white w-[100px] rounded-full h-[50px]"
-        onClick={handleClick}
-      >
-        {isImageVisible ? "Dar" : "Dar"}
-      </button>
+        <p className="text-2xl mb-[-50px]">
+          11Б анги Номин-Эрдэнэ, Энх-Үйлс, Анужин{" "}
+        </p>
+      </div>
     </div>
   );
 }
